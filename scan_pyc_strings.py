690099745743c50d9415433a7729761d430cdf8e
MuccMupa777 <kostya.type59@yandex.ru>
2026-03-27 13:37:21 +0300

    Update scan_pyc_strings.py

--- scan_pyc_strings.py
+++ scan_pyc_strings.py
@@ -1,19 +1,1 @@
-import sys
-
-P = r"C:\Users\User\Downloads\World_of_Tanks"
-b = open(P, "rb").read()
-
-subs = [
-	"g_cache", "gCache", "cache",
-	"init", "initialize", "load", "read",
-	"getArena", "getBy", "geometry", "geometryName",
-	"teamBasePositions", "teamBasePosition", "basePositions",
-	"minimap", "positions",
-	"ResMgr", "DataSection", "openSection",
-]
-
-for s in subs:
-	if s in b:
-		print("HIT", s)
-print("len", len(b))
 
